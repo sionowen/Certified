252204aad3717327edbc7235ec423660abad7e43
Alex Carufel <Carual42@gmail.com>
2022-11-06 21:46:53 -0600

    upload is getting 404 not found error

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+
+app.use(fileUpload());
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+    console.log('req.body', req.body)
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+const PORT = process.envPORT || 5007
+
+app.listen(PORT, () => console.log('Server Started on', PORT));
--- src/Components/ResumeUpload/ResumeUpload.jsx
+++ src/Components/ResumeUpload/ResumeUpload.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {Fragment, useState} from "react";
+import axios from "axios";
 
 
 function ResumeUpload() {
+    const[file, setFile] = useState('');
+    const[fileName, setFileName] = useState('Choose File');
+    const[uploadedfile, setUploadedFile] = useState({});
+
+    const onChange = e => {
+        setFile(e.target.files[0]);
+        setFileName(e.target.files[0].name);
+    }
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        const formData =new FormData();
+        formData.append('file', file)
+
+        try {
+    const res = await axios.post('/upload', formData, {
+    headers: {
+        'Content-Type': 'mulitpart/form-data'
+    }
+    });
+
+    const { fileName, filePath} = res.data;
+
+    setUploadedFile({ fileName, filePath})
+        }catch(err) {
+            if (err.response.status === 500) {
+                console.log('There was a problem with the server');
+              } else {
+                console.log('err in submit', err.response.data.msg);
+              }
+        }
+    };
+
   return (
-    <div className="row center">
-      <h3>upload here</h3>
+    <div className='container mt-4'>
+      <h3 className="display-4 text-center mb-4">upload here</h3>
+        <Fragment>
+            <form onSubmit={onSubmit}>
+            <div className="mb-3">
+  <label htmlFor="formFile" className="form-label">{fileName}</label>
+  <input className="form-control" type="file" id="formFile" onChange={onChange} />
+            </div>
+            <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4 mb-4" />
+            </form>
+        </Fragment>
+      
     </div>
   );
 }
