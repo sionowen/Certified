28f05c7654f4406f4af093bfad060c0ac238fe73
Alex Carufel <Carual42@gmail.com>
2022-11-13 16:12:11 -0600

    added social media to contact us page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -9,11 +9,15 @@ function Contact() {
         <p>Certified Anesthesia Care Inc.</p>
         <p>Address: 5751 Long Brake Circle, Edina MN 55439</p>
         <p>CEO: Michael Singh MSN, CRNA, APRN. </p>
-        <p>Business 651-257-2251</p>
-        <p>Cell 320-309-1161</p>
-        <p>Fax 866-726-9153</p>
+        <p>Business: 651-257-2251</p>
+        <p>Cell: 320-309-1161</p>
+        <p>Fax: 866-726-9153</p>
         <p>Business Manager: Jennifer Nelson</p>
-        <p>Jnelson@cac.health</p>
+        <p>Email: Jnelson@cac.health</p>
+        <div className="col-sm-4 col-sm-offset-2 col-xs-6 center patient-container-left">
+        <h3>Social media</h3>
+        <a href="https://www.linkedin.com/company/certified-anesthesia-care-inc/about/" class="text-warning">Linkedin</a>
+        </div>
       </div>
     </div>
   );
